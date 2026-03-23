b8593824a93ae29f7d3e9c74700e1b72b78b8a24
usapopopooon <usapopopooon@gmail.com>
2026-03-24 01:51:30 +0900

    CIのバージョンを揃える

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -1,10 +1,17 @@
+import path from "path";
+import { fileURLToPath } from "url";
 import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vitest/config";
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   resolve: {
-    tsconfigPaths: true,
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
   test: {
     environment: "jsdom",
